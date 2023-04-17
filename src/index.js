11bbc5e583d6c13242cd637b90d41a72a14fca8e
Lakshmi <lakshmi.prbh@gmail.com>
2023-04-16 20:01:22 -0600

    client done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { store,persistor } from './redux/store'; 
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {app} from "./firebase.config";
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<div>
+    <Provider store={store} app={app}>
+        <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+        </PersistGate>
+    </Provider>
+</div>
+,document.getElementById("root"));
+
+
+
